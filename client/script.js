5a41ad4644b5f0877cc47bff08b6c55b295a4a15
Gold-Eman <goldeman500@gmail.com>
2023-02-09 07:22:22 +0100

    All changes

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-chat-7cvg.onrender.com/', {
+    const response = await fetch('https://ai-chat-gygj.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
